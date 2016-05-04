cd3abf1cdd99fb03a3742970a93f117050d16acb
faddistr <faddistr@gmail.com>
2016-05-04 17:14:20 +0300

    Backup

--- branch.cpp
+++ branch.cpp
@@ -1,16 +1,36 @@
 #include "branch.h"
+#include <assert.h>
+#include <iterator>
 
 Branch::Branch(QJsonObject *config) {
 
+    if (config->contains("Transports")) {
+        if (config->value("Transports").isArray()) {
+            assert(makeTransports(config->value("Transports").toArray()) == 0);
+        }
+    }
+
+    if (config->contains("Handlers")) {
+        if (config->value("Handlers").isArray()) {
+            assert(makeHandlers(config->value("Transports").toArray()) == 0);
+        }
+    }
+
+    assert(makeConnections() == 0);
 }
 
 Branch::~Branch() {
+    QList<IConnector>::iterator i;
 
+    for (i = m_parts.begin(); i != m_parts.end(); i++) {
+        delete (*i);
+    }
+
+    m_parts.clear();
 }
 
 void Branch::addPart(IConnector *part) {
-
-
+    m_parts.append(part);
 }
 
 int Branch::makeTransports(QJsonArray *arr) {
